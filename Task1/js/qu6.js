e54efe59bef01a4ebf6ee0d31fe1ed316283ecca
AJoyceL <Lamajoyc3@gmail.com>
2026-01-14 15:29:42 -0500

    completed qu7

--- Task1/js/qu6.js
+++ Task1/js/qu6.js
@@ -12,8 +12,30 @@ function setup() {
 function draw() {
     background(0,0,0);
     
-    fill(255);
-    textSize(28);
-    text(test, 10, 50);
+
+    let d = 20;
+    for(let i = 0; i<10; i++){
+        const x = d * i + 20;
+        let y = 20;
+
+        fill(255);
+        textSize(16);
+        text(i, x, y);
+    }
+
+    for(let j = 0; j<16; j++){
+        const y = d * j + 20;
+        let x = 20; 
+
+        fill(255);
+        textSize(16);
+        text(j, x, y);
+    }    
+
+    // fill(255);
+    // textSize(28);
+    // text(test, 200, 200);
 }
 
+
+//Ask emile what the text is supposed to do, confused
---
+++ Task1/js/qu7.js
@@ -0,0 +1,54 @@
+"use strict";
+
+/**
+ * variabbles
+ */
+let circlegrid = {
+    x: 0,
+    d: 50,
+    r: 255,
+    g: 255,
+    b: 255,
+}
+
+let squaresgrid = false;
+
+function setup() {
+    createCanvas(400, 400);
+}
+
+function draw() {
+    background(0,0,0);
+    
+    let d = circlegrid.d; //diameter for the circles and grid size
+
+    for(let i = 0; i<10; i++){
+        const x = d * i + 25;
+
+        for(let j = 0; j<20; j++){
+            const y = d * j + 25;   
+            fill(circlegrid.r, circlegrid.g, circlegrid.b);
+            if(squaresgrid){
+                const x = d *i;
+                const y = d *j;
+                rect(x, y, circlegrid.d, circlegrid.d);
+            } else {
+                ellipse(x, y, circlegrid.d, circlegrid.d);
+            }
+        }
+    }    
+}
+
+function keyPressed(){
+    if(key === ' '){
+        circlegrid.r = random(0,255);
+        circlegrid.g = random(0,255);
+        circlegrid.b = random(0,255);
+    }   
+}
+
+function mousePressed(){
+    if(mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height){
+        squaresgrid = !squaresgrid;
+    }
+}
