0023d22c353c7dacb1c9219d7b70bf44ccdc478a
N0thing4O4 <LamAJoyc3@gmail.com>
2026-01-20 13:55:05 -0500

    added task2

---
+++ notes/note1/js/1_helloWorld.js
@@ -0,0 +1,4 @@
+document.write("<h2>HELLO WORLD DYNAMIC</h2>"); 
+// explanation: the use of document.write allows us to write directly to the HTML document from JavaScript. 
+//In which effectively outputs anything within the parentheses to the browser. In addition to placing a script in the body of a document, you can also put it in the head section. The location of any JavaScript code, (i.e within the head or body tags) is the default location on the page where JavaScript will write to.
+
