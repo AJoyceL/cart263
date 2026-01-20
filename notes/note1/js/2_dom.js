93dac8bf17b823beb008ae8cbc8190b7d8d5b323
N0thing4O4 <LamAJoyc3@gmail.com>
2026-01-20 16:03:55 -0500

    access element

--- notes/note1/js/2_dom.js
+++ notes/note1/js/2_dom.js
@@ -1,7 +1,8 @@
 
 window.onload = setup
-
 function setup(){
+    //*check elements in the console*/
+
     // console.log(document.getElementById("one"));
     // console.log(document.querySelector("#one"));
 
@@ -18,7 +19,39 @@ function setup(){
     // console.log(document.querySelectorAll(".square_shape"));
     // console.log("running setup");
 
+
+    // //modifying elements
+    console.log(document.querySelectorAll("span")[0].parentElement)
+    console.log(document.querySelector(".wrapper_flex_box").children)
+    document.querySelector("#two").children[0].innerHTML = "<h2> this is now a header</h2>";
+
+    //how to apply the change to all children if you haev multiple
+
+    // let childrenOfTwo = document.getElementById("two").children;
+    // for(let i = 0; i < childrenOfTwo.length; i++){
+    //     childrenOfTwo[i].innerHTML = "<h2>changed from JS</h2>";
+    // }
+
     
+//get the group
+let allSquareShapes = document.querySelectorAll(".square_shape");
+//go through each element
+for(let  singleSquareShape of allSquareShapes){
+    //get children
+   console.log(singleSquareShape.children[0])
+   singleSquareShape.children[0].textContent+="adding content"
+}
+
+
+//get the group
+let allSquareShapes = document.querySelectorAll(".square_shape");
+//go through each element
+for(let  singleSquareShape of allSquareShapes){
+    //get children
+    if(singleSquareShape.querySelector("p span")!== null){
+    singleSquareShape.querySelector("p span").textContent+= " other Content"
+    }
+}
 
 }
 
