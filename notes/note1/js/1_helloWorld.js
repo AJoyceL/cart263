4ae5a5e792a288870b89dbe76165597edba480ea
N0thing4O4 <LamAJoyc3@gmail.com>
2026-01-20 15:25:56 -0500

    started window by id

--- notes/note1/js/1_helloWorld.js
+++ notes/note1/js/1_helloWorld.js
@@ -1,4 +1,13 @@
-document.write("<h2>HELLO WORLD DYNAMIC</h2>"); 
+// document.write("<h2>HELLO WORLD DYNAMIC</h2>"); 
+
 // explanation: the use of document.write allows us to write directly to the HTML document from JavaScript. 
 //In which effectively outputs anything within the parentheses to the browser. In addition to placing a script in the body of a document, you can also put it in the head section. The location of any JavaScript code, (i.e within the head or body tags) is the default location on the page where JavaScript will write to.
 
+
+window.onload = setup
+function setup(){
+    console.log("running setup");
+    document.write("HELLO WORLD AFTER LOAD IN FUNCTION");
+}
+
+//Well first lets introduce the Window interface: which represents a window containing a DOM document. In contrast, the document property points to the DOM document loaded in that window. Next: The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets, scripts, iframes, and images... so... if we would like to incorporate a setup() like function in Vanilla JS. - we can assign a function to this load event.
---
+++ notes/note1/js/2_dom.js
@@ -0,0 +1,24 @@
+
+window.onload = setup
+
+function setup(){
+    // console.log(document.getElementById("one"));
+    // console.log(document.querySelector("#one"));
+
+    // console.log(document.getElementsByTagName("div"));
+
+    // let allDivs = document.getElementsByTagName("div");
+    // console.log(allDivs.length);
+    // // console.log(document.getElementsByTagName("div").length);
+    // // console.log(document.getElementsByTagName("div")[0]);
+  
+    // console.log(document.querySelectorAll("div"));
+
+    // console.log(document.getElementsByClassName(".square_shape"));
+    // console.log(document.querySelectorAll(".square_shape"));
+    // console.log("running setup");
+
+    
+
+}
+
