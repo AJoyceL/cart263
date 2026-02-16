8af648b1dc8d0a17b6b73bd5595444e2163bc5a8
AJoyceL <Lamajoyc3@gmail.com>
2026-02-16 16:51:33 -0500

    made bthe jour5nal entry editable by the user
    - Found contentEditable on W3schools and used it as reference for the entry

--- project1/js/script.js
+++ project1/js/script.js
@@ -5,14 +5,22 @@ function setup(){
     // let bounds = parentCanvas.getBoundingClientRect();
     // let offset = 20;
 
-    //changes date 
+    // changes date to real time date
     // https://www.geeksforgeeks.org/javascript/javascript-date-objects/
     const now = new Date();// default date cnstructor
     let formattedDate = now.toLocaleDateString('en-CA'); // sets the output format to YYYY-MM-DD
     document.querySelector("h3").textContent = formattedDate; 
     console.log(formattedDate); //automatically changes the date to match the day
     
-    //changes the text
-    document.querySelector("p").textContent = "Hi! this is the first entry of my journal. I've been trav3eling the world :3";
+    // changes the journal entry
+    // https://www.w3schools.com/jsref/prop_html_contenteditable.asp
+    let entry = document.getElementById("entry");
+    entry.addEventListener("click", journalEntry());
+    function journalEntry() {
+        entry.contentEditable = true;
+        entry.style.outline = "none";
+    };
+    
+
 
 }
