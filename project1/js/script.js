c795c4da4124c1cbb1b5ab06aeb1b63eca136aa4
AJoyceL <Lamajoyc3@gmail.com>
2026-02-16 17:41:01 -0500

    added a new page

--- project1/js/script.js
+++ project1/js/script.js
@@ -2,8 +2,6 @@ window.onload = setup;
 
 /** function setup */
 function setup(){
-    // let bounds = parentCanvas.getBoundingClientRect();
-    // let offset = 20;
 
     // changes date to real time date
     // https://www.geeksforgeeks.org/javascript/javascript-date-objects/
@@ -13,14 +11,45 @@ function setup(){
     console.log(formattedDate); //automatically changes the date to match the day
     
     // changes the journal entry
-    // https://www.w3schools.com/jsref/prop_html_contenteditable.asp
+    // contentEditable from: https://www.w3schools.com/jsref/prop_html_contenteditable.asp
     let entry = document.getElementById("entry");
     entry.addEventListener("click", journalEntry());
     function journalEntry() {
         entry.contentEditable = true;
+        entry
         entry.style.outline = "none";
+        // checkBounds(document.getElementById("parent"), entry);
     };
-    
 
+    //keeps entry within bounds of the parent div
+    function checkBounds(parent, p) {
+        let bounds = parent.getBoundingClientRect();
+        let pBounds = p.getBoundingClientRect();
+
+    }   
+
+    //creates a next button
+    let nextBtn = document.createElement("button");
+    nextBtn.textContent = "Next";
+    nextBtn.classList.add("nextBtn");
+    document.getElementById("parent").appendChild(nextBtn);
+    
+    function nextPage() {
+        // code to go to next page
+        let nextPageDiv = document.createElement("div");
+        nextPageDiv.classList.add("nextPage");
+        document.getElementById("parent").appendChild(nextPageDiv);
+
+        nextPageDiv.textContent = entry.textContent;
+    }
+    nextBtn.addEventListener("click", nextPage);
+
+    // if(nextBtn.clicked && nextPage()) {
+    //     entry.contentEditable = false;
+
+    //     let nextPageDiv = document.querySelector(".nextPage");
+    //     nextPageDiv.textContent = entry.textContent;
+        
+    // }
 
 }
