ffaf4a831c7dc1d7a9c7afeedc7845e0640742e1
N0thing4O4 <LamAJoyc3@gmail.com>
2026-02-17 15:47:00 -0500

    started API web storage

---
+++ notes/HTML_JS_3/js/localStorageTest.js
@@ -0,0 +1,12 @@
+let theButtons = document.getElementsByClassName("titleBar");
+for (let i = 0; i < theButtons.length; i++) {
+    theButtons[i].addEventListener("click", saveStateHandler);
+}
+
+// the callback function
+function saveStateHandler(event) {
+    console.log(this.parentElement); // get parent element
+    let buttonID = this.parentElement.id;
+    let inputValue = this.parentElement.querySelector("input").value;
+    console.log(inputValue);
+}
