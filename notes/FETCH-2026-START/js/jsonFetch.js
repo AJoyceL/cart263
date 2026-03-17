988e8c2750d0192dbe0b3fe066a993c23f834a6e
N0thing4O4 <LamAJoyc3@gmail.com>
2026-03-17 14:21:19 -0400

    finished fetch API

--- notes/FETCH-2026-START/js/jsonFetch.js
+++ notes/FETCH-2026-START/js/jsonFetch.js
@@ -1,6 +1,41 @@
 window.onload = goApp;
 
 async function goApp(){
-console.log("hello fetch")
+    console.log("hello fetch")
 
+    
+    window.onload = goFetch;
+    async function goFetch(){
+        try{
+            let response = await fetch('../files/tests.json'); //response
+            let data = await response.json();
+            console.log(data)
+            displayResults(data)
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    
+    function displayResults(parsedResultJS){
+        for (let i=0; i< parsedResultJS.length; i++){
+            console.log(parsedResultJS)
+            
+            //the object
+            let donutObj = parsedResultJS[i];
+            //container
+            let containerDiv = document.createElement("div");
+            containerDiv.classList.add("donutItem");
+            document.querySelector("#output").appendChild(containerDiv);
+        
+            let title = document.createElement("h3");
+            title.textContent = donutObj.name;
+            containerDiv.appendChild(title)
+
+            //access the image
+            let donutImage = document.createElement("img");
+            donutImage.src = donutObj.image;
+            containerDiv.appendChild(donutImage)
+        }
+    }
 }
