d62c1a73f3ab9f2a53d0835e1964759354b79196
AJoyceL <Lamajoyc3@gmail.com>
2026-04-15 13:24:43 -0400

    created a wall and table  for the room

--- project2/js/index.js
+++ project2/js/index.js
@@ -1,40 +1,79 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
-//texture loader
+/*
+   texture loader
+*/
+//from : https://polyhaven.com/a/brown_leather
 const textureLoader = new THREE.TextureLoader()
 const bookTexture = textureLoader.load('texture/brown_leather_rough_4k.jpg')
 
-//camera and scene setup
+/*
+   scene setup
+*/
 const scene = new THREE.Scene()
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
 
-//canvas setup
+/*
+   canvas setup
+*/
 const canvas = document.querySelector('canvas#three-ex')
 
-//camera setup
+
+
+/*
+   camera setup
+*/
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3;
-camera.position.y = -3;
-// camera.rotation.z = 5;
+camera.position.z = 2;
+camera.position.y = -1.5;
 scene.add(camera)
 
-//renderer setup
+
+
+/*
+   render setup
+*/
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
-//mesh setup => book
-const geometry = new THREE.BoxGeometry(1, 1.5, 0.2)
+
+
+/*
+   mesh setup
+*/
+
+//book
+const bookgeometry = new THREE.BoxGeometry(1, 1.5, 0.2)
 const material = new THREE.MeshBasicMaterial({ map: bookTexture })
-const book = new THREE.Mesh(geometry, material)
+material.color = new THREE.Color('#492904') 
+
+const book = new THREE.Mesh(bookgeometry, material)
 scene.add(book)
-// book.position.z = 5;
 book.rotation.z = 0.5;  
+
+//table
+const tablegeometry = new THREE.BoxGeometry(20, 0.5, 10)
+const tablematerial = new THREE.MeshBasicMaterial({ color: 0x654321 })
+const table = new THREE.Mesh(tablegeometry, tablematerial)
+scene.add(table)
+table.rotation.x = -1.6;
+table.position.z = -.3;
+
+//room wall
+const wallgeometry = new THREE.BoxGeometry(50, 10, 0.5)
+const wallmaterial = new THREE.MeshBasicMaterial({ color: 0x654321 })
+const wall = new THREE.Mesh(wallgeometry, wallmaterial)
+scene.add(wall)
+wall.position.z = -5;
+wall.position.y = 20;
+wall.rotation.x = 20;
+
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
