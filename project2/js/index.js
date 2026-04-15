6fb84f53dbfef21384201fd8e165c8f40ce10b73
AJoyceL <Lamajoyc3@gmail.com>
2026-04-15 13:06:25 -0400

    added leather texture to book + fixed position

--- project2/js/index.js
+++ project2/js/index.js
@@ -1,27 +1,40 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
+//texture loader
+const textureLoader = new THREE.TextureLoader()
+const bookTexture = textureLoader.load('texture/brown_leather_rough_4k.jpg')
+
 //camera and scene setup
 const scene = new THREE.Scene()
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
 }
+
+//canvas setup
 const canvas = document.querySelector('canvas#three-ex')
+
+//camera setup
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 10;
+camera.position.z = 3;
+camera.position.y = -3;
+// camera.rotation.z = 5;
 scene.add(camera)
 
+//renderer setup
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 
-//create mesh:book
+//mesh setup => book
 const geometry = new THREE.BoxGeometry(1, 1.5, 0.2)
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+const material = new THREE.MeshBasicMaterial({ map: bookTexture })
 const book = new THREE.Mesh(geometry, material)
 scene.add(book)
+// book.position.z = 5;
+book.rotation.z = 0.5;  
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
@@ -31,9 +44,6 @@ controls.enableDamping = true;
 window.requestAnimationFrame(animate);
 
 function animate(timer) {
-    // // book.rotation.x = Math.sin(timer/1000)
-    // book.rotation.y = Math.sin(timer/1000)
-    // controls.target.y = 0.5
 
     renderer.render(scene, camera); 
     window.requestAnimationFrame(animate);
